d700ad93995927ac6edaf496b8949e728722b440
Administrator <admin@example.com>
2018-05-24 13:21:52 +0800

    修改了添加主机时自动建立ssh 连接

---
+++ auto/bin/vba_extract.py
@@ -0,0 +1,5 @@
+#!/root/automatic/auto/bin/python
+# EASY-INSTALL-SCRIPT: 'XlsxWriter==1.0.2','vba_extract.py'
+__requires__ = 'XlsxWriter==1.0.2'
+import pkg_resources
+pkg_resources.run_script('XlsxWriter==1.0.2', 'vba_extract.py')
